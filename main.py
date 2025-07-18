d73a3e07efe12c317786af395f2c9be8f36d6e7c
Fabián1820 <fernandezfabian2003@gmail.com>
2025-07-18 11:13:44 -0400

    Implement main entry point and update Python SDK version to 3.13
    - Added a main entry point to run the application using Uvicorn.
    - Updated the project SDK version from Python 3.12 to 3.13 in configuration files.

--- main.py
+++ main.py
@@ -90,5 +90,11 @@ app.include_router(
     tags=["Compartidos"]
 )
 
-
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        app,
+        host="127.0.0.1",
+        port=8000
+    )
 
